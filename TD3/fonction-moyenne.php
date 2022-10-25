48720828500d481f089161968dc5b4de24729d87
Julien-Costa-Castro <julien.costa-castro@etu.umontpellier.fr>
2022-10-25 09:42:28 +0200

    TD3

--- TD3/fonction-moyenne.php
+++ TD3/fonction-moyenne.php
@@ -1,12 +1,10 @@
 <?php
 function moyenne(array $n):float{
-    $q = 0;
-    $p = 0;
-    $c = 0;
-    while(count($n) != $q){
-        $p = $n[$q] + $p;
-        $q++;
+    $somme = 0;
+    foreach($n as $v){
+        $somme += $v;
     }
-    $c = $p / count($n);
-    return $c;
+    return $somme/count($n);
 }
+
+
